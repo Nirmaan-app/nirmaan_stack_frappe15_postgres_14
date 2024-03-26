e02a5024001fee9afab1bfec099e6b6d27e8818a
Ashutosh Chaubey <ashutoshchaubeymail@gmail.com>
2024-03-26 11:14:41 +0000

    adding project form in /projects/edit route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { FrappeProvider } from 'frappe-react-sdk'
-import { Button } from './components/ui/button'
+// import { Button } from './components/ui/button'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Dashboard from './pages/dashboard'
 import Projects from './pages/projects'
 import Customers from './pages/customers'
 import WorkPackages from './pages/work-packages'
+import EditProject from './pages/edit-project'
 //import { useStickyState } from './hooks/useStickyState'
 import { ThemeProvider } from './components/theme-provider'
 import { ProtectedRoute } from './utils/auth/ProtectedRoute'
@@ -24,7 +25,7 @@ const router = createBrowserRouter(
 				<Route index element={<Dashboard />} />
 				<Route path="projects" >
 					<Route index element={<Projects />} />
-					{/* <Route path="edit" element={<EditProject />} /> */}
+					<Route path="edit" element={<EditProject />} /> 
 					{/* <Route
 						path=":projectId"
 						// loader={(({ params }) => {
---
+++ frontend/src/pages/edit-project.tsx
@@ -0,0 +1,42 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@/components/breadcrumb"
+import { NavBar } from "@/components/nav/nav-bar"
+import { ProjectForm }  from "@/components/project-form"
+import { Separator } from "@/components/ui/separator"
+
+export default function EditProject(){
+    return (
+        <>
+            <NavBar />
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <div className="flex items-center justify-between space-y-2">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/projects">Projects</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem isCurrentPage>
+                            <BreadcrumbLink href="/projects/edit">
+                                Edit Project
+                            </BreadcrumbLink>
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
+                <div className="space-y-0.5">
+                    <h2 className="text-2xl font-bold tracking-tight">Add Project</h2>
+                    <p className="text-muted-foreground">
+                        Fill out this to save a new project
+                    </p>
+                </div>
+                <Separator className="my-6" />
+                <div className="space-y-6">
+                    <ProjectForm />
+                </div>
+            </div>
+
+
+
+        </>
+    )
+}
