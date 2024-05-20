20085eebd4d0ccb9e6d5976c336f82c67327b259
Abhishek Kumar <abhishekism0010@gmail.com>
2024-05-20 07:43:48 +0000

    fix: AlertTriangle import fix

--- frontend/src/components/common/callouts/error-callouts.tsx
+++ frontend/src/components/common/callouts/error-callouts.tsx
@@ -1,12 +1,12 @@
 import { PropsWithChildren } from "react"
-import { TriangleAlert } from 'lucide-react';
+import { AlertTriangle } from 'lucide-react';
 import { CustomCallout } from "./custom-callout"
 
 
 export const ErrorCallout = ({ children, ...props }: PropsWithChildren<{ message?: string }>) => {
     return (<CustomCallout
         rootProps={{ color: "red" }}
-        iconChildren={<TriangleAlert size={18} />}
+        iconChildren={<AlertTriangle size={18} />}
         textChildren={children || props.message || "An error occurred"}
     />)
 }
