2ccf8897a22058ca0a640669f454b9d9b6d124a1
Ashutosh Chaubey <ashutoshchaubeymail@gmail.com>
2024-05-31 10:18:45 +0000

    fix: added shadcn data tables

--- frontend/src/pages/approve-select-sent-back.tsx
+++ frontend/src/pages/approve-select-sent-back.tsx
@@ -2,6 +2,18 @@ import { MainLayout } from "@/components/layout/main-layout";
 import { useFrappeGetDocList } from "frappe-react-sdk";
 import { Link } from "react-router-dom";
 import { useUserData } from "@/hooks/useUserData";
+import { useMemo } from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTable } from "@/components/data-table/data-table";
+import { DataTableColumnHeader } from "@/components/data-table/data-table-column-header";
+
+
+type PRTable = {
+    name: string
+    project_name: string
+    creation: string
+    category: string
+}
 
 export const ApproveSelectSentBack = () => {
     const userData = useUserData();
@@ -12,13 +24,111 @@ export const ApproveSelectSentBack = () => {
         });
     const getTotal = (order_id: string) => {
             let total:number = 0;
-            const orderData = sent_back_list?.find(item => item.name === order_id).item_list;
+            const orderData = sent_back_list?.find(item => item.name === order_id)?.item_list;
             orderData?.list.map((item) => {
                 const price = item.quote;
                 total += price ? parseFloat(price) : 0;
             })
             return total;
         }
+
+        const columns: ColumnDef<PRTable>[] = useMemo(
+            () => [
+                {
+                    accessorKey: "name",
+                    header: ({ column }) => {
+                        return (
+                            <DataTableColumnHeader column={column} title="ID" />
+                        )
+                    },
+                    cell: ({ row }) => {
+                        return (
+                            <div className="font-medium">
+                                <Link className="underline hover:underline-offset-2" to={`/approve-sent-back/${row.getValue("name")}`}>
+                                    {row.getValue("name")}
+                                </Link>
+                            </div>
+                        )
+                    }
+                },
+                {
+                    accessorKey: "procurement_request",
+                    header: ({ column }) => {
+                        return (
+                            <DataTableColumnHeader column={column} title="PR Number" />
+                        )
+                    },
+                    cell: ({ row }) => {
+                        return (
+                            <div className="font-medium">
+                                {row.getValue("procurement_request")?.slice(-4)}
+                            </div>
+                        )
+                    }
+                },
+                {
+                    accessorKey: "creation",
+                    header: ({ column }) => {
+                        return (
+                            <DataTableColumnHeader column={column} title="Date" />
+                        )
+                    },
+                    cell: ({ row }) => {
+                        return (
+                            <div className="font-medium">
+                                {row.getValue("creation")?.split(" ")[0]}
+                            </div>
+                        )
+                    }
+                },
+                {
+                    accessorKey: "project_name",
+                    header: ({ column }) => {
+                        return (
+                            <DataTableColumnHeader column={column} title="Project" />
+                        )
+                    },
+                    cell: ({ row }) => {
+                        return (
+                            <div className="font-medium">
+                                {row.getValue("project_name")}
+                            </div>
+                        )
+                    }
+                },
+                {
+                    accessorKey: "category",
+                    header: ({ column }) => {
+                        return (
+                            <DataTableColumnHeader column={column} title="Category" />
+                        )
+                    },
+                    cell: ({ row }) => {
+                        return (
+                            <div className="font-medium">
+                                {row.getValue("category")}
+                            </div>
+                        )
+                    }
+                },
+                {
+                    accessorKey: "total",
+                    header: ({ column }) => {
+                        return (
+                            <DataTableColumnHeader column={column} title="Estimated Price" />
+                        )
+                    },
+                    cell: ({ row }) => {
+                        return (
+                            <div className="font-medium">
+                                {getTotal(row.getValue("name"))}
+                            </div>
+                        )
+                    }
+                }
+            ],
+            []
+        )
 
     return (
         <MainLayout>
@@ -27,8 +137,9 @@ export const ApproveSelectSentBack = () => {
                     <div className="flex items-center justify-between space-y-2">
                         <h2 className="text-lg font-bold tracking-tight">Approve Vendors</h2>
                     </div>
-                    {/* <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2"> */}
-                    <div className="overflow-x-auto">
+                    
+                    <DataTable columns={columns} data={sent_back_list || []} />
+                    {/* <div className="overflow-x-auto">
                         <table className="min-w-full divide-gray-200">
                             <thead className="bg-gray-50">
                                 <tr>
@@ -58,7 +169,7 @@ export const ApproveSelectSentBack = () => {
                                 ))}
                             </tbody>
                         </table>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </MainLayout>
