161da39cdb2917a5d74c7e7294cef752a1bfe56e
Abhishek Kumar <abhishekism0010@gmail.com>
2024-06-07 06:40:09 +0000

    feat: tested useFrapperFileUpload for image uploads to backend

--- frontend/src/pages/debug.tsx
+++ frontend/src/pages/debug.tsx
@@ -1,25 +1,67 @@
-import { useFrappeGetDocList } from "frappe-react-sdk"
+import { Button } from "@/components/ui/button";
+import { useFrappeFileUpload, useFrappeGetDocList, useFrappeUpdateDoc } from "frappe-react-sdk"
+import { useState } from "react";
 import { Link } from "react-router-dom"
 
 export default function Debug() {
-    const { data: user_data, isLoading: user_loading, error: user_error } = useFrappeGetDocList("User")
+    const { upload: upload_img, loading: upload_loading, error: upload_error, reset: upload_reset } = useFrappeFileUpload()
+    const { updateDoc: update, loading: update_loading, error: update_error } = useFrappeUpdateDoc()
+    const [file, setFile] = useState(); //Your File object
+    const [preview, setPreview] = useState(); //Your File object
+
+    const fileArgs = {
+        /** If the file access is private then set to TRUE (optional) */
+        "isPrivate": true,
+        /** Folder the file exists in (optional) */
+        "folder": "Home",
+        /** Doctype associated with the file (optional) */
+        "doctype": "Items",
+        /** Docname associated with the file (mandatory if doctype is present) */
+        "docname": "ITEM-000006",
+        /** Field to be linked in the Document **/
+        "fieldname": "image_url"
+    }
+    function handleChange(e) {
+        console.log(e.target.files);
+        setPreview(URL.createObjectURL(e.target.files[0]));
+        setFile(e.target.files[0]);
+    }
+
+    function saveImage() {
+        upload_img(
+            file,
+            fileArgs,
+            'frappe.handler.upload_file'
+        )
+            .then((doc) => update('Items', "ITEM-000006", {
+                image_url: doc.file_url,
+            }))
+            .catch(e => console.error(e))
+
+    }
+
+
     return (
         <>
-            {user_error && <h1>ERROR</h1>}
-            {user_loading ? <p>Loading</p> : user_data?.map(item => <><Link key={item.name + "_l"} to="/debug">{item.name}</Link><br /><DebugRoles key={item.name} user={item.name} /></>)}
+            <h2>Add Image:</h2>
+            <input type="file" onChange={handleChange} />
+            <img src={preview} />
+            <button onClick={saveImage}>Save</button>
+            {/* {user_error && <h1>ERROR</h1>}
+            {user_loading ? <p>Loading</p> : user_data?.map(item => <><Link key={item.name + "_l"} to="/debug">{item.name}</Link><br /><DebugRoles key={item.name} user={item.name} /></>)} */}
         </>
     )
 }
 
-function DebugRoles(props: { user: string }) {
-    const { data: role_data, isLoading: role_loading, error: role_error } = useFrappeGetDocList("Has Role", {
-        filters: [["parent", "=", props.user]]
-    })
+// function DebugRoles(props: { user: string }) {
+//     const { data: role_data, isLoading: role_loading, error: role_error } = useFrappeGetDocList("Has Role", {
+//         filters: [["parent", "=", props.user]]
+//     })
 
-    return (
-        <>
-            {role_error && <h1>ERROR</h1>}
-            {role_loading ? <p>Loading</p> : role_data?.map(item => <p>{item.name}</p>)}
-        </>
-    )
-}
+//     return (
+//         <>
+//             {role_error && <h1>ERROR</h1>}
+//             {role_loading ? <p>Loading</p> : role_data?.map(item => <p>{item.name}</p>)}
+//         </>
+//     )
+// }
