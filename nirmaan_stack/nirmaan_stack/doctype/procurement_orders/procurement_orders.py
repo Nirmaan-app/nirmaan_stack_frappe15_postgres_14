eea0f7f4e1b1ccb2677c5de4f1c473fd626fa87b
Abhishek Kumar <abhishekism0010@gmail.com>
2024-07-08 06:45:06 +0000

    fix: generalized PO serial number

--- nirmaan_stack/nirmaan_stack/doctype/procurement_orders/procurement_orders.py
+++ nirmaan_stack/nirmaan_stack/doctype/procurement_orders/procurement_orders.py
@@ -3,7 +3,15 @@
 
 # import frappe
 from frappe.model.document import Document
+from frappe.model.naming import getseries
+from datetime import datetime
 
 
 class ProcurementOrders(Document):
-	pass
+	def autoname(self):
+		project = self.project.split("-")[-1]
+		curr_month = datetime.now().month
+		curr_year = datetime.now().year
+		fin_yr = str(curr_year)[2:]+"-"+str(curr_year+1)[2:] if curr_month>3 else str(curr_year-1)[2:]+"-"+str(curr_year)[2:] 
+		prefix = project
+		self.name = f"PO/{getseries(prefix, 3)}/{project}/{fin_yr}"
