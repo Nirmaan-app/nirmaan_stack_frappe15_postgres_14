03e175cf6523e4ec590233d36642a44dbf80bc3c
Abhishek Kumar <abhishekism0010@gmail.com>
2024-08-14 12:07:37 +0000

    feat: added a patch to update all existing PO's with tax fields

---
+++ nirmaan_stack/patches/v1_6/add_tax_to_po.py
@@ -0,0 +1,19 @@
+import frappe
+
+def execute():
+   """
+   Adding category and tax to PO's order list
+   """ 
+   pos = frappe.get_all("Procurement Orders")
+   for po in pos:
+         doc = frappe.get_doc("Procurement Orders", po)
+         new_list = {'list': []}
+         for item in doc.order_list['list']:
+              it = frappe.get_doc('Items', item['name'])
+              cat = frappe.get_doc('Category', it.category)
+              obj = item
+              obj['category']=cat.name
+              obj['tax'] = int(cat.tax)
+              new_list['list'].append(obj)
+         frappe.db.set_value("Procurement Orders", po, "order_list", str(new_list).replace('\'','\"'))
+   frappe.db.commit()
