a0339d9c24b21df0e92a8987037b10886ebf1af1
Abhishek Kumar <abhishekism0010@gmail.com>
2024-09-17 07:19:25 +0000

    fix: added notes in PO doctype

--- frontend/src/types/NirmaanStack/ProcurementOrders.ts
+++ frontend/src/types/NirmaanStack/ProcurementOrders.ts
@@ -16,6 +16,32 @@ export interface ProcurementOrders{
 	vendor?: string
 	/**	Procurement Request : Link - Procurement Requests	*/
 	procurement_request?: string
+	/**	Category : Link - Category	*/
+	category?: string
 	/**	Attachment : Data	*/
 	attachment?: string
+	/**	Project Name : Data	*/
+	project_name?: string
+	/**	Project Address : Data	*/
+	project_address?: string
+	/**	Vendor Name : Data	*/
+	vendor_name?: string
+	/**	Vendor Address : Data	*/
+	vendor_address?: string
+	/**	Vendor GST : Data	*/
+	vendor_gst?: string
+	/**	Order List : JSON	*/
+	order_list?: any
+	/**	Category List : JSON	*/
+	category_list?: any
+	/**	Advance : Data	*/
+	advance?: string
+	/**	Loading Charges : Data	*/
+	loading_charges?: string
+	/**	Freight Charges : Data	*/
+	freight_charges?: string
+	/**	Notes : Data	*/
+	notes?: string
+	/**	Status : Data	*/
+	status?: string
 }
