df5ca15b7f816eaf95190432b609a202225888e2
Abhishek Kumar <abhishekism0010@gmail.com>
2024-10-01 13:40:52 +0000

    fix: added Nirmaan Comments Doctype

---
+++ nirmaan_stack/nirmaan_stack/doctype/nirmaan_comments/test_nirmaan_comments.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Nirmaan (Stratos Infra Technologies Pvt. Ltd.) and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestNirmaanComments(FrappeTestCase):
+	pass
