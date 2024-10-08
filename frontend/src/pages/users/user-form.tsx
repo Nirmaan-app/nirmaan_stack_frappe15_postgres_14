49785827b0bb2ecd3bcaac3245d1a9a4f8ec24f1
Abhishek Kumar <abhishekism0010@gmail.com>
2024-10-08 09:35:47 +0000

    fix: ui fixes for users

--- frontend/src/pages/users/user-form.tsx
+++ frontend/src/pages/users/user-form.tsx
@@ -106,136 +106,136 @@ export const UserForm = () => {
 
     return (
         <div className="flex-1">
-                <div className="flex gap-2">
-                    <ArrowLeft className="mt-1.5 cursor-pointer" onClick={() => navigate("/users")} />
-                        <div className="flex flex-col">
-                            <h2 className="text-xl md:text-3xl font-bold tracking-tight">Add User</h2>
-                            <p className="text-muted-foreground">
-                                Fill out to create a new User
-                            </p>
-                        </div>
+            <div className="flex gap-2">
+                <ArrowLeft className="mt-1.5 cursor-pointer" onClick={() => navigate("/users")} />
+                <div className="flex flex-col">
+                    <h2 className="text-xl md:text-3xl font-bold tracking-tight">Add New User</h2>
+                    <p className="text-muted-foreground">
+                        Fill all the marked details to create a new User
+                    </p>
                 </div>
-                
+            </div>
+
             <Separator className="my-6 max-md:my-2" />
             <Form {...form}>
                 <form onSubmit={(event) => {
                     event.stopPropagation();
                     return form.handleSubmit(onSubmit)(event);
                 }} className="px-6 max-md:px-2 flex flex-col gap-4">
-                        <p className="text-sky-600 font-semibold">User Details</p>
-                        <FormField
-                            control={form.control}
-                            name="first_name"
-                            render={({ field }) => (
-                                <FormItem className="lg:flex lg:items-center gap-4">
-                                    <FormLabel className="md:basis-2/12">First Name<sup>*</sup></FormLabel>
-                                    <div className="flex flex-col items-start md:basis-2/4">
-                                        <FormControl className="">
-                                            <Input placeholder="First Name" {...field} />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </div>
-                                    <FormDescription>
-                                        Example: John
-                                    </FormDescription>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="last_name"
-                            render={({ field }) => (
-                                <FormItem className="lg:flex lg:items-center gap-4">
-                                    <FormLabel className="md:basis-2/12">Last Name<sup>*</sup></FormLabel>
-                                    <div className="flex flex-col items-start md:basis-2/4">
-                                        <FormControl className="">
-                                            <Input placeholder="Last Name" {...field} />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </div>
-                                    <FormDescription>
-                                        Example: Doe
-                                    </FormDescription>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="mobile_no"
-                            render={({ field }) => (
-                                <FormItem className="lg:flex lg:items-center gap-4">
-                                    <FormLabel className="md:basis-2/12">Mobile Number<sup>*</sup></FormLabel>
-                                    <div className="flex flex-col items-start md:basis-2/4">
-                                        <FormControl className="">
-                                            <Input placeholder="Mobile Number" {...field} />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </div>
-                                    <FormDescription>
-                                        Example: 9999999999
-                                    </FormDescription>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="email"
-                            render={({ field }) => (
-                                <FormItem className="lg:flex lg:items-center gap-4">
-                                    <FormLabel className="md:basis-2/12">Email<sup>*</sup></FormLabel>
-                                    <div className="flex flex-col items-start md:basis-2/4">
-                                        <FormControl className="">
-                                            <Input placeholder="Email" {...field} />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </div>
-                                    <FormDescription>
-                                        Example: john@doe.in
-                                    </FormDescription>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="role_profile_name"
-                            render={({ field }) => (
-                                <FormItem className="lg:flex lg:items-center gap-4">
-                                        <FormLabel className="md:basis-2/12">Role Profile<sup>*</sup></FormLabel>
-                                    <div className="md:basis-2/4">
-                                            <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                            <div className="flex flex-col items-start">
-                                                <FormControl>
-                                                    <SelectTrigger>
-                                                        <SelectValue placeholder="Select the Role" />
-                                                    </SelectTrigger>
-                                                </FormControl>
-                                                <FormMessage />
-                                                </div>
-                                                <SelectContent>
-                                                    {role_profile_list_loading && <div>Loading...</div>}
-                                                    {role_profile_list_error && <div>Error: {role_profile_list_error.message}</div>}
-                                                    {options.map(option => (
-                                                        <SelectItem value={option.value}>{option.label}</SelectItem>
-                                                    ))}
+                    <p className="text-sky-600 font-semibold">User Details</p>
+                    <FormField
+                        control={form.control}
+                        name="first_name"
+                        render={({ field }) => (
+                            <FormItem className="lg:flex lg:items-center gap-4">
+                                <FormLabel className="md:basis-2/12">First Name<sup>*</sup></FormLabel>
+                                <div className="flex flex-col items-start md:basis-2/4">
+                                    <FormControl className="">
+                                        <Input placeholder="First Name" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </div>
+                                <FormDescription>
+                                    Example: John
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="last_name"
+                        render={({ field }) => (
+                            <FormItem className="lg:flex lg:items-center gap-4">
+                                <FormLabel className="md:basis-2/12">Last Name<sup>*</sup></FormLabel>
+                                <div className="flex flex-col items-start md:basis-2/4">
+                                    <FormControl className="">
+                                        <Input placeholder="Last Name" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </div>
+                                <FormDescription>
+                                    Example: Doe
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="mobile_no"
+                        render={({ field }) => (
+                            <FormItem className="lg:flex lg:items-center gap-4">
+                                <FormLabel className="md:basis-2/12">Mobile Number<sup>*</sup></FormLabel>
+                                <div className="flex flex-col items-start md:basis-2/4">
+                                    <FormControl className="">
+                                        <Input placeholder="Mobile Number" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </div>
+                                <FormDescription>
+                                    Example: 9999999999
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem className="lg:flex lg:items-center gap-4">
+                                <FormLabel className="md:basis-2/12">Email<sup>*</sup></FormLabel>
+                                <div className="flex flex-col items-start md:basis-2/4">
+                                    <FormControl className="">
+                                        <Input placeholder="Email" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </div>
+                                <FormDescription>
+                                    Example: john@doe.in
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="role_profile_name"
+                        render={({ field }) => (
+                            <FormItem className="lg:flex lg:items-center gap-4">
+                                <FormLabel className="md:basis-2/12">Role Profile<sup>*</sup></FormLabel>
+                                <div className="md:basis-2/4">
+                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                        <div className="flex flex-col items-start">
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Select the Role" />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <FormMessage />
+                                        </div>
+                                        <SelectContent>
+                                            {role_profile_list_loading && <div>Loading...</div>}
+                                            {role_profile_list_error && <div>Error: {role_profile_list_error.message}</div>}
+                                            {options.map(option => (
+                                                <SelectItem value={option.value}>{option.label}</SelectItem>
+                                            ))}
 
-                                                </SelectContent>
-                                            </Select>
-                                            </div>
-                                            <FormDescription>
-                                                Role associated with this User
-                                            </FormDescription>
-                                </FormItem>
-                            )}
-                        />
-                        <div className="flex items-center justify-end lg:justify-center">
-                            {loading ? (
-                                <ButtonLoading />
-                            ) : (
-                                <Button className="flex items-center gap-1" type="submit">
-                                    <ListChecks className="h-4 w-4" />
-                                    Submit</Button>
-                            )}
-                        </div>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                                <FormDescription>
+                                    Role associated with this User
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                    />
+                    <div className="flex items-center justify-end lg:justify-center">
+                        {loading ? (
+                            <ButtonLoading />
+                        ) : (
+                            <Button className="flex items-center gap-1" type="submit">
+                                <ListChecks className="h-4 w-4" />
+                                Submit</Button>
+                        )}
+                    </div>
                 </form>
             </Form>
         </div>
