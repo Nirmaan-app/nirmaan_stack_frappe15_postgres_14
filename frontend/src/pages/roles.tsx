ba394a8add7325403aad5d2557b7b917f4287307
Abhishek Kumar <abhishekism0010@gmail.com>
2025-01-02 08:54:03 +0000

    chore: removed navbar from roles.tsx

--- frontend/src/pages/roles.tsx
+++ frontend/src/pages/roles.tsx
@@ -64,7 +64,6 @@ export default function Users() {
     )
     return (
         <>
-            <NavBar />
             <div className="flex-1 space-y-4 p-8 pt-6">
                 <div className="flex items-center justify-between space-y-2">
                     <Breadcrumb>
