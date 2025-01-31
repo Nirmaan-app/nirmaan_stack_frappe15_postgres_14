20f9f90f8e1830c4ae59ac2e33899707710f5676
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2025-01-31 14:50:39 +0000

    chore: added approved quotes card to estimates exec dashboard, and fixed a minor bug in doc count store

--- frontend/src/components/estimates-executive-dashboard.tsx
+++ frontend/src/components/estimates-executive-dashboard.tsx
@@ -1,4 +1,4 @@
-import {  HardHat, ShoppingCart, Truck } from "lucide-react";
+import {  Coins, HardHat, ShoppingCart, Truck } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { CardHeader, CardTitle, Card, CardContent } from "./ui/card";
@@ -22,7 +22,7 @@ export const EstimatesExecutive = () => {
 
     const permissionsList = projectPermissions?.map((i) => i?.for_value)
 
-    const { data: projectsData, mutate: projectsDataMutate, isLoading: projectsDataLoading } = useFrappeGetDocList("Projects", {
+    const { data: projectsData, isLoading: projectsDataLoading } = useFrappeGetDocList("Projects", {
         fields: ["*"],
         filters: [["name", "in", permissionsList || []]],
         limit: 10000
@@ -30,13 +30,15 @@ export const EstimatesExecutive = () => {
         (user_id === "Administrator" || !permissionsList) ? null : undefined
     )
 
+    const { data: approved_quotes, isLoading: approved_quotes_loading } = useFrappeGetDocCount("Approved Quotations");
+
     return (
         <>
             <div className="flex-1 space-y-4">
                 <div className="flex items-center justify-between space-y-2">
                     <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
                 </div>
-                <Card className="hover:animate-shadow-drop-center" data-cy="admin-dashboard-project-card" >
+                <Card className="hover:animate-shadow-drop-center">
                         <Link to="/projects">
                             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                                 <CardTitle className="text-sm font-medium">
@@ -48,6 +50,23 @@ export const EstimatesExecutive = () => {
                                 <div className="text-2xl font-bold">
                                     {(projectsDataLoading) ? (<TailSpin visible={true} height="30" width="30" color="#D03B45" ariaLabel="tail-spin-loading" radius="1" wrapperStyle={{}} wrapperClass="" />)
                                         : (projectsData?.length)}
+                                </div>
+                                {/* <p className="text-xs text-muted-foreground">COUNT</p> */}
+                            </CardContent>
+                        </Link>
+                </Card>
+                <Card className="hover:animate-shadow-drop-center">
+                        <Link to="/approved-quotes">
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                    Approved Quotations
+                                </CardTitle>
+                                <Coins className="h-4 w-4 text-muted-foreground" />
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">
+                                    {(approved_quotes_loading) ? (<TailSpin visible={true} height="30" width="30" color="#D03B45" ariaLabel="tail-spin-loading" radius="1" wrapperStyle={{}} wrapperClass="" />)
+                                        : (approved_quotes)}
                                 </div>
                                 {/* <p className="text-xs text-muted-foreground">COUNT</p> */}
                             </CardContent>
