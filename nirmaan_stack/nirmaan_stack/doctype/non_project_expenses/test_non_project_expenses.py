61fb41455357680768ecba36a6a9a3cb3f47de83
Abhishek Kumar <abhishekism0010@gmail.com>
2025-06-23 15:19:14 +0000

    fix: added missing doctype file

---
+++ nirmaan_stack/nirmaan_stack/doctype/non_project_expenses/test_non_project_expenses.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Nirmaan (Stratos Infra Technologies Pvt. Ltd.) and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestNonProjectExpenses(FrappeTestCase):
+	pass
