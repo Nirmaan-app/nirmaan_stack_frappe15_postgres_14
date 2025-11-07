c1a7d71c5b1d577c324831e3ecd3f6805dcddbc2
Abhishek Kumar <abhishekism0010@gmail.com>
2025-11-07 03:46:35 +0000

    fix: changes sentry tracking to stack

--- frontend/src/instrument.ts
+++ frontend/src/instrument.ts
@@ -12,8 +12,8 @@ Sentry.init({
     tracesSampleRate: 0.2,
     // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
     tracePropagationTargets: [
-        "test.nirmaan.app",
-        /^https:\/\/test\.nirmaan\.app/,
+        "stack.nirmaan.app",
+        /^https:\/\/stack\.nirmaan\.app/,
     ],//["localhost", /^https:\/\/yourserver\.io\/api/],
     sendDefaultPii: true,
     // Enable logs to be sent to Sentry
