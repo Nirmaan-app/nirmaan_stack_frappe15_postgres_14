554ef55feb7a1f59f31ff066219924122b2f46ef
Madhu Balan T <madhubalan1247@gmail.com>
2026-02-02 12:11:12 +0530

    feat: Update default search fields for PO and SR reports to vendor name .

--- frontend/src/pages/reports/config/poAttachmentReconcileTable.config.ts
+++ frontend/src/pages/reports/config/poAttachmentReconcileTable.config.ts
@@ -1,9 +1,9 @@
 import { SearchFieldOption } from '@/components/data-table/new-data-table';
 
 export const PO_ATTACHMENT_RECONCILE_SEARCHABLE_FIELDS: SearchFieldOption[] = [
-    { value: "name", label: "PO ID", placeholder: "Search by PO ID...", default: true },
-    { value: "projectName", label: "Project", placeholder: "Search by Project..." },
-    { value: "vendorName", label: "Vendor", placeholder: "Search by Vendor..." },
+    { value: "name", label: "PO ID", placeholder: "Search by PO ID..."},
+    { value: "projectName", label: "Project", placeholder: "Search by Project..."},
+    { value: "vendorName", label: "Vendor", placeholder: "Search by Vendor...", default: true  },
 ];
 
 export const PO_ATTACHMENT_RECONCILE_DATE_COLUMNS: string[] = ["creation", "latestDeliveryDate"];
